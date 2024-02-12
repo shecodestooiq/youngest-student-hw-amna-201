87ef9a120e60ac48cd0f17de48bdc02f3c7d0d91
amna mohamed <81439598+amna-201@users.noreply.github.com>
2024-02-12 16:47:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function showYoungestStudent(students) { }
+var students=[
+    {name:'Amna',age:23},
+    {name:'Loly',age:22},
+    {name:'Sura',age:23},
+    {name:'Nona',age:16},
+
+];
+
+var minuma= Math.min(...students.map(b=>b.age));
+var minName= students.find(students=>students.age==minuma);
+
+function showYoungestStudent(students) {
+
+    
+    console.log('the Name of the youngest student: ');
+    console.log(minName.name);
+    
+ }
+ showYoungestStudent();
 
 module.exports = showYoungestStudent;
